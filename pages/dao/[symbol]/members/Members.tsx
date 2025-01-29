dafebca22eaefea5517ce7b2f931feca6173ae78
0xodia <0xodia@solend.fi>
2025-01-28 18:04:34 -0800

    fix build by not rendering on error

--- pages/dao/[symbol]/members/Members.tsx
+++ pages/dao/[symbol]/members/Members.tsx
@@ -44,9 +44,9 @@ const Members = () => {
 
   const { connection } = useConnection()
   const realmPk = useSelectedRealmPubkey()
-  const { data: activeMembers, error } = useMembersQuery()
+  const { data: activeMembers, isError, isLoading, isLoadingError } = useMembersQuery()
 
-  if (error) return null;
+  if (isError || isLoading || isLoadingError) return null;
 
   const { result: kind } = useAsync(async () => {
     if (realmPk === undefined) return undefined
