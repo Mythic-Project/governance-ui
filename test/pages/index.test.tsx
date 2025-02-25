6abf679a5c800c1008dd9eda5af3f156edf71e94
RastDjuss <Folienesie@google.com>
2025-02-24 19:40:31 -0500

    maDE it  through the yarn install dependencies warning list and added missing dependencies, adjusted some versions and added all peer deps... probably have to afit version conflicts now

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -5,7 +5,24 @@ import React from 'react'
 import Home from '../../pages/index'
 import { render, waitFor } from '@testing-library/react'
 import singletonRouter from 'next/router'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
+const Index = () => {
+  const router = useRouter();
+  const REALM = process.env.REALM;
+
+  useEffect(() => {
+    const mainUrl = REALM ? `/dao/${REALM}` : '/realms';
+    if (!router.asPath.includes(mainUrl)) {
+      router.replace(mainUrl);
+    }
+  }, [REALM, router]);
+
+  return null;
+};
+
+export default Index;
 const originalRealm = process.env.REALM
 
 describe('Home page redirects to', () => {
